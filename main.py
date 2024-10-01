dcc8564ecc6b6ce0e4d214ad605a74a190d1be8e
User-xxU000 <158011297+User-xxU000@users.noreply.github.com>
2024-10-01 13:10:31 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+
+
+st.set_page_config(page_title="Scripts For roblox - Free Robux Code Generator 2024 💛✨")
+
+st.markdown(
+    "<style> img { border-radius: 10px; transition: transform 0.3s; } img:hover { transform: translateY(-5px); } </style>", 
+    unsafe_allow_html=True
+)
+st.title("""Scripts For roblox - Free Robux Code Generator 2024 💛✨""")
+col1,col2,col3 = st.columns(3)
+
+st.header("""Robux Generators Code: Features and Benefits
+
+Program Description:
+“Robux Generators Code” is a program designed for Roblox fans that offers a way to get Robux quickly by using automated scripts. This program connects to the Roblox platform and exploits vulnerabilities to increase users' Robux balance. However, it can also be considered risky due to Roblox's security policies.""")
+
+
+with col1:
+    image = st.image("kj.png", width=800)
+st.success("Some of the links may be down, so in this section you have 3 options with which you can try some of them.")
+boton = st.link_button("Option 1 Link",url ="https://link-target.net/1238954/roblox-scripts-robux")
+boton2 = st.link_button("Option 2 Link",url ="https://link-target.net/1238954/roblox-scripts-robux")
+boton3 = st.link_button("Option 3 Link",url ="https://link-target.net/1238954/roblox-scripts-robux")
